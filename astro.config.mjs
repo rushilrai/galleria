74ff11dcb3a7b10bfcda61e54c47ece3cced1e61
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2025-04-11 00:58:35 +0530

    change output directory;

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    outDir: './build',
     prefetch: {
         prefetchAll: true
     },
