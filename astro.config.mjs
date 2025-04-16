56d04a6d161d37f066950a401a4886c6043fbbf5
Rushil Rai <58342835+rushilrai@users.noreply.github.com>
2025-04-17 00:50:33 +0530

    prettier working; logo files added; Header init;

--- astro.config.mjs
+++ astro.config.mjs
@@ -3,6 +3,7 @@ import { defineConfig } from 'astro/config';
 import tailwindcss from '@tailwindcss/vite';
 
 export default defineConfig({
+    site: "https://www.rushilrai.me",
     outDir: './build',
     prefetch: {
         prefetchAll: true
